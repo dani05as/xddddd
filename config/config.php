d0262ea25dcb46e6c63487130fa9bfc3e9920386
dani05as <90471997+dani05as@users.noreply.github.com>
2021-09-10 21:04:58 -0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "[Database Username]");
+define("DB_PASS", "[Database Password]");
+define("DB_NAME", "[Database Name]");
+define("ADMIN_EMAIL", "[Admin Email]");
+define("APP_NAME", "BlackNET");
+define("APP_DEVELOPER", "Black.Hacker");
+define("APP_VERSION", "v3.6.0");
+define("APP_PATH", dirname(__FILE__, 2) . DIRECTORY_SEPARATOR); // ( Don't Change );
+define("DATA_SPLITTER", "|BN|"); // ( Don't Change );
